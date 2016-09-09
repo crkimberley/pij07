7713fff7ac0e019f2e6930b5c46c1007f867f55f
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-09-09 02:19:29 +0100

    HospitalDoublyLinked & PatientDoublyLinked classes - up & down printing implemented - now in a single recursive method

--- src/HospitalDoublyLinked.java
+++ src/HospitalDoublyLinked.java
@@ -32,14 +32,14 @@ public class HospitalDoublyLinked {
         PatientDoublyLinked p10 = new PatientDoublyLinked("John", 21, "Mumps");
         patientListStart.addPatient(p10);
 
-        printPatients(patientListStart);
+        printPatients(patientListStart, true);
 
-        System.out.println("Delete a couple of patients...");
+        System.out.println("\nDelete a couple of patients...");
         patientListStart.deletePatient(p5);
         patientListStart.deletePatient(p7);
-        printPatients(patientListStart);
+        printPatients(patientListStart, true);
 
-        System.out.println("Delete 7 more patients...");
+        System.out.println("\nDelete 7 more patients...");
         patientListStart.deletePatient(p2);
         patientListStart.deletePatient(p3);
         patientListStart.deletePatient(p4);
@@ -47,37 +47,37 @@ public class HospitalDoublyLinked {
         patientListStart.deletePatient(p8);
         patientListStart.deletePatient(p9);
         patientListStart.deletePatient(p10);
-        printPatients(patientListStart);
+        printPatients(patientListStart, true);
 
-        System.out.println("Demonstrate we can't delete firstPatient from inside the list");
+        System.out.println("\nDemonstrate we can't delete firstPatient from inside the list");
         patientListStart.deletePatient(firstPatient);
-        printPatients(patientListStart);
+        printPatients(patientListStart, true);
 
-        System.out.println("Delete firstPatient using deleteFirstPatient()");
+        System.out.println("\nDelete firstPatient using deleteFirstPatient()");
         deleteFirstPatient();
-        printPatients(patientListStart);
+        printPatients(patientListStart, true);
     }
 
-    private void printPatients(PatientDoublyLinked patient) {
+    private void printPatients(PatientDoublyLinked patient, boolean up) {
+        if (patient == patientListStart && up) {
+            System.out.println("========================================");
+        }
         if (patient == null) {
+            System.out.println("========================================");
             return;
         }
-        if (patient.getNextPatient() == null) {
-            printPatientsDown(patient);
-            return;
+        if (up) {
+            if (patient.getNextPatient() == null) {
+                up = false;
+                printPatients(patient, up);
+                System.out.println(" LENGTH = " + listLengthRecursive(patientListStart) + " (recursive)"
+                        + " " + listLengthIterative(patientListStart) + " (iterative)");
+                System.out.println("========================================");
+                return;
+            }
         }
         System.out.printf(" %-20s%-6d%12s\n", patient.getName(), patient.getAge(), patient.getIllness());
-        printPatients(patient.getNextPatient());
-    }
-
-    private void printPatientsDown(PatientDoublyLinked patient) {
-        if (patient == null) {
-            System.out.println(" length = " + listLengthRecursive(patientListStart) + " (recursive)"
-                    + " " + listLengthIterative(patientListStart) + " (iterative)");
-            return;
-        }
-        System.out.printf(" %-20s%-6d%12s\n", patient.getName(), patient.getAge(), patient.getIllness());
-        printPatientsDown(patient.getPreviousPatient());
+        printPatients((up ? patient.getNextPatient() : patient.getPreviousPatient()), up);
     }
 
     private void deleteFirstPatient() {
